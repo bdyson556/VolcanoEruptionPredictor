edeaabd12dce63854f5786c43eb57f80b72ea368
bdyson556 <71849300+bdyson556@users.noreply.github.com>
2024-02-20 22:46:24 -0600

    Finished basic CSV mapping tasks. Outputting to single CSV. Next step is to further engineer features if necessary and split the data.

--- preprocessor/PreprocessorForCSV.py
+++ preprocessor/PreprocessorForCSV.py
@@ -4,11 +4,11 @@ from preprocessor.PreProcessor import PreProcessor
 
 class PreprocessorForCSV(PreProcessor):
 
-
     def __init__(self):
         super().__init__()
         self.map_function = self.map_record_to_CSV
         self.output_data_object = []
+        self.current_final_tilt = None
 
 
     def output_to_CSV(self):
@@ -36,6 +36,9 @@ class PreprocessorForCSV(PreProcessor):
                     elif key == "tilt_erupt":
                         value = float(value.replace("nrad", ""))
                         record.append(value)
+                        if observ_id == max_seconds_for_current_volcano:
+                            self.current_final_tilt = value
+                        record.append(self.current_final_tilt)
         # assert (len(record) == 12)
         record_list.append(record)
         return record_list
